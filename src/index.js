470928cabea02282cb6799b4a9fbc0c078a8733a
yuvalkarif3 <yuvalkarif3@gmail.com>
2021-09-28 17:19:41 +0300

    cors options

--- src/index.js
+++ src/index.js
@@ -8,16 +8,22 @@ import helmet from "helmet";
 
 //Initiating Express
 const app = express();
-app.use(helmet());
-app.use(compression());
-
-//Allowing other sites to fetch
-
 app.use(
   cors({
     origin: "https://yuvalkarif.github.io",
   })
 );
+app.options(
+  "*",
+  cors({
+    origin: "https://yuvalkarif.github.io",
+  })
+);
+app.use(helmet());
+app.use(compression());
+
+//Allowing other sites to fetch
+
 //Parsing the Body for Post Requests
 app.use(express.json());
 app.use(express.static("./public"));
@@ -28,16 +34,6 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 //Connecting to MongoDB
 const db = mongoDB();
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "https://yuvalkarif.github.io");
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  next();
-});
 app.use("/api", apiRouter);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, function () {
